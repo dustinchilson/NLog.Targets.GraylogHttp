3c472111f73e708a2c39999abc52bf0139b5e1f4
Dustin Chilson <dustinchilson@users.noreply.github.com>
2017-06-30 13:09:54 -0400

    Working example for NetCore1.1, NetFx is a WIP

---
+++ examples/netFx/NLogGraylogHttp.Example.NetFx/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace NLogGraylogHttp.Example.NetFx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nLogLogger = LogManager.GetCurrentClassLogger();
+
+            while (true)
+            {
+                nLogLogger.Trace("NLog Logging - Trace");
+                nLogLogger.Debug("NLog Logging - Debug");
+                nLogLogger.Info("NLog Logging - Info");
+                nLogLogger.Warn("NLog Logging - Warn");
+                nLogLogger.Error("NLog Logging - Error");
+                nLogLogger.Fatal("NLog Logging - Fatal");
+            }
+        }
+    }
+}
---
+++ examples/netcore/NLogGraylogHttp.Example.NetCore/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using NLog;
+using NLog.Extensions.Logging;
+
+namespace NLogGraylogHttp.Example.NetCore
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .BuildServiceProvider();
+
+            serviceProvider
+                .GetService<ILoggerFactory>()
+                .AddNLog()
+                .ConfigureNLog("NLog.config");
+
+            var logger = serviceProvider.GetService<ILoggerFactory>().CreateLogger<Program>();
+            var nLogLogger = LogManager.GetCurrentClassLogger();
+
+            while (true)
+            {
+                logger.LogTrace("Extensions.Logging - LogTrace");
+                logger.LogDebug("Extensions.Logging - LogDebug");
+                logger.LogInformation("Extensions.Logging - LogInformation");
+                logger.LogWarning("Extensions.Logging - LogWarning");
+                logger.LogError("Extensions.Logging - LogError");
+                logger.LogCritical("Extensions.Logging - LogCritical");
+
+                nLogLogger.Trace("NLog Logging - Trace");
+                nLogLogger.Debug("NLog Logging - Debug");
+                nLogLogger.Info("NLog Logging - Info");
+                nLogLogger.Warn("NLog Logging - Warn");
+                nLogLogger.Error("NLog Logging - Error");
+                nLogLogger.Fatal("NLog Logging - Fatal");
+            }
+        }
+    }
+}
