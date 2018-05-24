ea5167b98809c90cd15470cb11f3f7dbbfad8847
Dustin Chilson <dustin.chilson@Salespad.com>
2018-05-24 14:40:47 -0400

    Fix nuget restore

--- tools/before_build.ps1
+++ tools/before_build.ps1
@@ -2,4 +2,4 @@ if(Test-Path .\artifacts) {
 	Remove-Item .\artifacts -Force -Recurse 
 }
 
-dotnet restore src\NLog.Targets.GraylogHttp\NLog.Targets.GraylogHttp.csproj
+dotnet restore NLog.Targets.GraylogHttp.sln
