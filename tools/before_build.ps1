0c4a452b397f18cd92663e36d7a4560ca60963b5
Dustin Chilson <dustin.chilson@Salespad.com>
2018-05-25 08:44:49 -0400

    Finish build script move

--- tools/before_build.ps1
+++
@@ -1,5 +0,0 @@
-if(Test-Path .\artifacts) { 
-	Remove-Item .\artifacts -Force -Recurse 
-}
-
-dotnet restore NLog.Targets.GraylogHttp.sln
--- tools/init.ps1
+++
@@ -1,4 +0,0 @@
-if ($env:APPVEYOR_REPO_BRANCH -ne "master")
-{
-	Update-AppveyorBuild -Version "$($env:APPVEYOR_REPO_COMMIT.substring(0,7))"
-}
