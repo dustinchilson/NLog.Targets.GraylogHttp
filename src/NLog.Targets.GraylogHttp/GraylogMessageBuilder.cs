3ebc135a8677fd4692c82642ad282d3a9bab093d
Rolf Kristensen <sweaty1@hotmail.com>
2018-05-25 20:24:16 +0200

    Added support for structured logging properties (IncludeEventProperties = true)

--- src/NLog.Targets.GraylogHttp/GraylogMessageBuilder.cs
+++ src/NLog.Targets.GraylogHttp/GraylogMessageBuilder.cs
@@ -1,76 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace NLog.Targets.GraylogHttp
 {
     internal class GraylogMessageBuilder
     {
         private readonly JsonObject _graylogMessage = new JsonObject();
-        private readonly Func<int> _timestampGenerator;
-
-        internal GraylogMessageBuilder(Func<int> timestampGenerator = null)
-        {
-            if (timestampGenerator == null)
-                timestampGenerator = () => (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
-
-            _timestampGenerator = timestampGenerator;
-        }
+        private static readonly DateTime _epochTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
         public GraylogMessageBuilder WithLevel(LogLevel level)
         {
-            GelfLevel graylogLevel;
+            object graylogLevel;
 
-            if (level == LogLevel.Debug)
-                graylogLevel = GelfLevel.Debug;
-            else if (level == LogLevel.Fatal)
-                graylogLevel = GelfLevel.Critical;
+            if (level == LogLevel.Trace)
+                graylogLevel = GelfLevel_Debug;
+            else if (level == LogLevel.Debug)
+                graylogLevel = GelfLevel_Debug;
             else if (level == LogLevel.Info)
-                graylogLevel = GelfLevel.Informational;
-            else if (level == LogLevel.Trace)
-                graylogLevel = GelfLevel.Informational;
+                graylogLevel = GelfLevel_Informational;
+            else if (level == LogLevel.Warn)
+                graylogLevel = GelfLevel_Warning;
+            else if (level == LogLevel.Fatal)
+                graylogLevel = GelfLevel_Critical;
             else
-                graylogLevel = level == LogLevel.Warn ? GelfLevel.Warning : GelfLevel.Error;
+                graylogLevel = GelfLevel_Error;
 
-            return WithProperty("level", (int)graylogLevel);
+            return WithProperty("level", graylogLevel);
         }
 
         public GraylogMessageBuilder WithProperty(string propertyName, object value)
         {
-            // Trunate due to https://github.com/Graylog2/graylog2-server/issues/873
-            // 32766b max, C# strings 2b per char
-            _graylogMessage[propertyName] = value.ToString().Truncate(16383);
+            if (value is IConvertible primitive && primitive.GetTypeCode() != TypeCode.String)
+            {
+                value = primitive;
+            }
+            else
+            {
+                // Trunate due to https://github.com/Graylog2/graylog2-server/issues/873
+                // 32766b max, C# strings 2b per char
+                value = value?.ToString()?.Truncate(16383);
+            }
+
+            _graylogMessage[propertyName] = value;
             return this;
         }
 
         public GraylogMessageBuilder WithCustomProperty(string propertyName, object value)
         {
-            return WithProperty($"_{propertyName}", value);
+            if (!propertyName.StartsWith("_", StringComparison.Ordinal))
+                propertyName = string.Concat("_", propertyName);
+
+            return WithProperty(propertyName, value);
         }
 
-        public GraylogMessageBuilder WithCustomPropertyRange(Dictionary<string, string> properties)
+        public string Render(DateTime timestamp)
         {
-            return properties.Aggregate(this, (builder, pair) => builder.WithCustomProperty(pair.Key, pair.Value));
-        }
-
-        public string Render()
-        {
-            WithProperty("timestamp", _timestampGenerator());
+            WithProperty("timestamp", (long)(timestamp.ToUniversalTime() - _epochTime).TotalSeconds);
             WithProperty("version", "1.1");
-
             return _graylogMessage.ToString();
         }
 
-        public enum GelfLevel
-        {
-            Emergency = 0,
-            Alert = 1,
-            Critical = 2,
-            Error = 3,
-            Warning = 4,
-            Notice = 5,
-            Informational = 6,
-            Debug = 7
-        }
+        //private static readonly object GelfLevel_Emergency = 0;
+        //private static readonly object GelfLevel_Alert = 1;
+        private static readonly object GelfLevel_Critical = 2;
+        private static readonly object GelfLevel_Error = 3;
+        private static readonly object GelfLevel_Warning = 4;
+        //private static readonly object GelfLevel_Notice = 5;
+        private static readonly object GelfLevel_Informational = 6;
+        private static readonly object GelfLevel_Debug = 7;
     }
 }
