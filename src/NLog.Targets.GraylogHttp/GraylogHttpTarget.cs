995218d29fee45b0fcec585c8efaf54cfe7b6901
Dustin Chilson <dustinchilson@users.noreply.github.com>
2019-01-23 15:22:07 -0500

    Add a circuit breaker for the connection to the graylog server. (#49)
    The connection to the Graylog server can go or be mis-configured
    if this happens its doesn't make sense to continue to send messages
    to a server that cannot be reached. This commit pauses messages
    from being sent to the server temporarily.

--- src/NLog.Targets.GraylogHttp/GraylogHttpTarget.cs
+++ src/NLog.Targets.GraylogHttp/GraylogHttpTarget.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using NLog.Common;
 using NLog.Config;
+using Polly;
+using Polly.CircuitBreaker;
 
 namespace NLog.Targets.GraylogHttp
 {
     [Target("GraylogHttp")]
     public class GraylogHttpTarget : TargetWithContext
     {
+        private HttpClient _httpClient;
+        private Uri _requestAddress;
+        private CircuitBreakerPolicy _policy;
+
         public GraylogHttpTarget()
         {
             ContextProperties = new List<TargetPropertyWithContext>();
@@ -19,21 +27,23 @@ namespace NLog.Targets.GraylogHttp
         [RequiredParameter]
         public string GraylogServer { get; set; }
 
-        [RequiredParameter]
         public string GraylogPort { get; set; }
 
-        [RequiredParameter]
         public string Facility { get; set; }
 
         public string Host { get; set; }
 
+        public int FailureThreshold { get; set; } = 50;
+
+        public int SamplingDurationSeconds { get; set; } = 10;
+
+        public int MinimumThroughput { get; set; } = 10;
+
+        public int FailureCooldownSeconds { get; set; } = 30;
+
         [ArrayParameter(typeof(TargetPropertyWithContext), "parameter")]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1721:PropertyNamesShouldNotMatchGetMethods", Justification = "NLog Behavior")]
         public override IList<TargetPropertyWithContext> ContextProperties { get; }
-
-        private HttpClient _httpClient;
-
-        private Uri _requestAddress;
 
         protected override void InitializeTarget()
         {
@@ -41,13 +51,25 @@ namespace NLog.Targets.GraylogHttp
                 Host = GetMachineName();
 
             _httpClient = new HttpClient();
-            _requestAddress = new Uri(string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0}:{1}/gelf", GraylogServer, GraylogPort));
+
+            _requestAddress = new Uri(string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0}/gelf", GraylogServer));
+
+            if (!string.IsNullOrEmpty(GraylogPort))
+                _requestAddress = new Uri(string.Format(System.Globalization.CultureInfo.InvariantCulture, "{0}:{1}/gelf", GraylogServer, GraylogPort));
+
             _httpClient.DefaultRequestHeaders.ExpectContinue = false; // Expect (100) Continue breaks the graylog server
 
-            // Prefix the custom properties with underscore upfront, so we dont have to do it for each logevent
-            for (int i = 0; i < ContextProperties.Count; ++i)
+            _policy = Policy
+                .Handle<Exception>()
+                .AdvancedCircuitBreaker(
+                    (double)FailureThreshold / 100,
+                    TimeSpan.FromSeconds(SamplingDurationSeconds),
+                    MinimumThroughput,
+                    TimeSpan.FromSeconds(FailureCooldownSeconds));
+
+            // Prefix the custom properties with underscore upfront, so we don't have to do it for each log-event
+            foreach (TargetPropertyWithContext p in ContextProperties)
             {
-                var p = ContextProperties[i];
                 if (!p.Name.StartsWith("_", StringComparison.Ordinal))
                     p.Name = string.Concat("_", p.Name);
             }
@@ -57,12 +79,14 @@ namespace NLog.Targets.GraylogHttp
 
         protected override void Write(LogEventInfo logEvent)
         {
-            GraylogMessageBuilder messageBuilder = new GraylogMessageBuilder()
-                .WithCustomProperty("_facility", Facility)
+             GraylogMessageBuilder messageBuilder = new GraylogMessageBuilder()
                 .WithProperty("short_message", logEvent.FormattedMessage)
                 .WithProperty("host", Host)
                 .WithLevel(logEvent.Level)
-                .WithCustomProperty("_logger_name", logEvent.LoggerName);
+                .WithCustomProperty("logger_name", logEvent.LoggerName);
+
+             if (!string.IsNullOrEmpty(Facility))
+                 messageBuilder.WithCustomProperty("facility", Facility);
 
             var properties = GetAllProperties(logEvent);
             foreach (var property in properties)
@@ -93,31 +117,29 @@ namespace NLog.Targets.GraylogHttp
 
             try
             {
-                // Ensure to reuse the same HttpClient. See also: https://aspnetmonsters.com/2016/08/2016-08-27-httpclientwrong/
-                _httpClient.PostAsync(_requestAddress, new StringContent(messageBuilder.Render(logEvent.TimeStamp), Encoding.UTF8, "application/json")).Result.EnsureSuccessStatusCode();
+                _policy.Execute(() =>
+                {
+                    var content = new StringContent(messageBuilder.Render(logEvent.TimeStamp), Encoding.UTF8, "application/json");
+                    return _httpClient.PostAsync(_requestAddress, content).Result.EnsureSuccessStatusCode();
+                });
             }
-            catch (AggregateException ex)
+            catch (BrokenCircuitException)
             {
-                foreach (var inner in ex.Flatten().InnerExceptions)
-                    InternalLogger.Error(inner, "GraylogHttp(Name={0}): Fail to post LogEvents", Name);
-            }
-            catch (Exception ex)
-            {
-                InternalLogger.Error(ex, "GraylogHttp(Name={0}): Fail to post LogEvents", Name);
+                InternalLogger.Error("GraylogHttp(Name={0}): The Graylog server seems to be inaccessible, the log messages were not sent to the server.", Name);
             }
         }
 
         /// <summary>
         /// Gets the machine name
         /// </summary>
-        private static string GetMachineName()
+        private string GetMachineName()
         {
             return TryLookupValue(() => Environment.GetEnvironmentVariable("COMPUTERNAME"), "COMPUTERNAME")
                 ?? TryLookupValue(() => Environment.GetEnvironmentVariable("HOSTNAME"), "HOSTNAME")
                 ?? TryLookupValue(() => Dns.GetHostName(), "DnsHostName");
         }
 
-        private static string TryLookupValue(Func<string> lookupFunc, string lookupType)
+        private string TryLookupValue(Func<string> lookupFunc, string lookupType)
         {
             try
             {
@@ -126,7 +148,7 @@ namespace NLog.Targets.GraylogHttp
             }
             catch (Exception ex)
             {
-                InternalLogger.Warn(ex, "GraylogHttp: Failed to lookup {0}", lookupType);
+                InternalLogger.Warn(ex, "GraylogHttp(Name={0}): Failed to lookup {1}", Name, lookupType);
                 return null;
             }
         }
