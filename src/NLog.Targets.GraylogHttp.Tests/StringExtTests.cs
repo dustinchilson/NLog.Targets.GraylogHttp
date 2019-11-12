78847a3cbd209f53d04b1888806157fd4a68fe95
dustinchilson <dustinchilson@users.noreply.github.com>
2019-11-11 20:25:00 -0500

    Fix build with new version of Sonar

--- src/NLog.Targets.GraylogHttp.Tests/StringExtTests.cs
+++ src/NLog.Targets.GraylogHttp.Tests/StringExtTests.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
+﻿using Xunit;
 
 namespace NLog.Targets.GraylogHttp.Tests
 {
     public class StringExtTests
     {
         [Fact]
-#pragma warning disable CA1822 // Mark members as static
         public void TruncateTest()
-#pragma warning restore CA1822 // Mark members as static
         {
             var testString = new string('*', 50000).Truncate(1);
             Assert.Equal("*", testString);
--- src/NLog.Targets.GraylogHttp/GraylogHttpTarget.cs
+++ src/NLog.Targets.GraylogHttp/GraylogHttpTarget.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using NLog.Common;
 using NLog.Config;
 using Polly;
