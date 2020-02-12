b0a1202472acadf68327fd723b08c248634c05f1
mathieu Chateau <mathieu.chateau@lotp.fr>
2020-02-12 16:15:15 +0100

    applied strong name

--- src/NLog.Targets.GraylogHttp/Properties/AssemblyInfo.cs
+++ src/NLog.Targets.GraylogHttp/Properties/AssemblyInfo.cs
@@ -9,4 +9,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2da00b6b-a170-419d-ad82-ac139745a11e")]
 
-[assembly: InternalsVisibleTo("NLog.Targets.GraylogHttp.Tests")]
+[assembly: InternalsVisibleTo("NLog.Targets.GraylogHttp.Tests,PublicKey=002400000480000094" +
+                              "000000060200000024000052534131000400000100010019c68d6e929d6ede5d8ab513c70095c611bcf4befbbdd2918" +
+                              "0c2760ebc93bad3b0eb006b1252c1f34f4e53e9bf77c3773fbac38f9f5194a1602ee5fb2807a84ae0956e6f15e44b682504" +
+                              "620cec6f07b4b43122a59f569d89dea652ff0b283dd016e7b73e421afe65e8bdd3008c7b48811619b9341179ae861c1ee8dd3bfcd9bd")]
